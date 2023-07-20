4181931ba105cc0451e320c54061e94808d10204
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2023-07-20 09:09:54 +0100

    Updated svg folder

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,7 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title','body','user_id', 'tags'];
+    protected $fillable = ['title','body','user_id'];
 
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
